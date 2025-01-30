2c0861f6c866f872893e88410a1ca3549a2241cd
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-01-30 04:51:18 +0300

    Initial commit, part 2

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import static java.lang.System.console;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author developer
+ */
+public class Main {
+
+    /**
+     * Main class, where 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
