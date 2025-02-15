4454332739b25881f637fe28702e3db4a3dc1d3e
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-02-15 15:39:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("There's nothing for now, come back later");
     }
 }
